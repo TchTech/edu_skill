d4c8bfd54954681147b0a8f3ff176d291ccc01b0
Alexonys1 <107358560+Alexonys1@users.noreply.github.com>
2023-03-15 22:41:13 +0300

    Отправляю файлы перед сном.
    Код работает в Яндекс Диалогах.
    Закиньте эти три файлика и проверьте изменения (ага, легко сказать: "Закинь").

--- json_manager.py
+++ json_manager.py
@@ -1,9 +1,36 @@
 import json
+import random
 
 
 with open ("texts_for_users.json", "r") as file:
     texts_for_users = json.load (file)
 
-
 def get_text_for_new_user ( ) -> str:
     return texts_for_users["for_new_user"]
+
+def get_hello_text ( ) -> str:
+    return random.choice (texts_for_users["hello_text"])
+
+def get_text_asking_if_user_has_forgotten_commands ( ) -> str:
+    return random.choice (texts_for_users["if_user_has_forgotten_commands"])
+
+def get_main_commands_of_skill ( ) -> str:
+    return texts_for_users["main_commands_of_skill"]
+
+def get_text_that_says_which_lesson_user_stopped (lesson_number: int) -> str:
+    return random.choice (texts_for_users["you_stopped_at_lesson"]) + str (lesson_number)
+
+def get_theme_of_lesson (lesson_number: int) -> str:
+    return "ЗАТЫЧКА"
+    #return lessons[str(lesson_number)]["theme"]
+
+def get_apology_text ( ) -> str:
+    return random.choice (texts_for_users["something_is_unclear"])
+    # Выбирается одно случайное извинение от Алисы, если она что-то не поняла.
+
+def get_farewell_text ( ) -> str:
+    head_of_farewell: str = random.choice (texts_for_users["head_of_farewell"])
+    body_of_farewell: str = random.choice (texts_for_users["body_of_farewell"])
+    return head_of_farewell + body_of_farewell
+    # Достаётся начало прощания, например: "До свидания";
+    # а потом тело прощания, например: ", хорошего дня!"
--- main.py
+++ main.py
@@ -12,31 +12,45 @@ class HandlerOfAlisa:
         self._SESSION_DATA: dict = request["state"]["session"]
         self._INTERSESSIONAL_DATA: dict = request["state"]["user"] # Межсессионные данные
         self._create_intersessional_data_if_necessary ( )
-        self._make_new_variables ( )
+        self._prepare_correct_answer ( )
 
     def _create_intersessional_data_if_necessary (self) -> None:
         # Я думаю, что у нового пользователя межсессионные данные пусты,
-        # поэтому делаю создателя этих данных, если это необходимо
-        try:
+        # поэтому создаю их, если это необходимо
+        if "new_user" in self._INTERSESSIONAL_DATA.keys ( ):
             self._IS_NEW_USER: bool = self._INTERSESSIONAL_DATA["new_user"]
-        except KeyError:
+        else:
             self._IS_NEW_USER = True
+            self._INTERSESSIONAL_DATA["new_user"] = True
             self._INTERSESSIONAL_DATA["last_lesson"] = 0
             self._INTERSESSIONAL_DATA["last_task"] = 0
 
-    def _make_new_variables (self) -> None:
+    def _prepare_correct_answer (self) -> None:
         if self._IS_NEW_USER:
             self._NEW_TEXT = get_text_for_new_user ( )
             self._IS_NEW_USER = False
             self._INTERSESSIONAL_DATA["new_user"] = False
+        
+        elif self._IS_FIRST_MESSAGE:
+            self._NEW_TEXT = get_hello_text ( ) + "\n"
+            self._NEW_TEXT += get_text_asking_if_user_has_forgotten_commands ( ) + "\n"
+            self._NEW_TEXT += get_main_commands_of_skill ( )
 
+        # Пока навык разрабатывается
         elif self._has_all_words_in_text_in_lower ("сбрось", "настройки"):
             self._IS_NEW_USER = True
             self._INTERSESSIONAL_DATA["new_user"] = True
+            self._INTERSESSIONAL_DATA["last_lesson"] = 0
+            self._INTERSESSIONAL_DATA["last_task"] = 0
             self._NEW_TEXT = "Настройки сброшены."
         
-        elif self._has_all_words_in_text_in_lower ("извинись"):
-            self._NEW_TEXT = get_apology_text ( )
+        elif self._has_one_word_in_text_in_lower (
+            "курс", "курса", "курсу", "курсе", "курсом"):
+            last_lesson: int = self._INTERSESSIONAL_DATA["last_lesson"]
+            self._NEW_TEXT = get_text_that_says_which_lesson_user_stopped (last_lesson) + "\n"
+            self._NEW_TEXT += "Его тема: " + get_theme_of_lesson (last_lesson) + "\n"
+            self._NEW_TEXT += "Хотите перейти к следующему уроку?"
+            # FIXME
         
         elif self._has_one_word_in_text_in_lower (
             "пока", "пака", "прощай", "свидания", "пора", "чао"):
@@ -44,10 +58,7 @@ class HandlerOfAlisa:
             self._IS_END_SESSION = True
 
         else:
-            self._NEW_TEXT = self._ORIGINAL_TEXT
-
-        self._NEW_SESSION_DATA = self._SESSION_DATA
-        self._NEW_INTERSESSIONAL_DATA = self._INTERSESSIONAL_DATA
+            self._NEW_TEXT = get_apology_text ( )
 
     def _has_all_words_in_text_in_lower (self, *words) -> bool:
         for word in words:
@@ -66,8 +77,8 @@ class HandlerOfAlisa:
         return {
             "version": self._VERSION,
             "response": {"text": self._NEW_TEXT, "end_session": self._IS_END_SESSION},
-            "session_state": self._NEW_SESSION_DATA,
-            "user_state_update": self._NEW_INTERSESSIONAL_DATA
+            "session_state": self._SESSION_DATA,
+            "user_state_update": self._INTERSESSIONAL_DATA
             }
 
 
