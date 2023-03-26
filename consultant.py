dd37d64178db5ef04f684a974034d9b422038d56
TchTech <67865526+TchTech@users.noreply.github.com>
2023-03-26 13:52:57 +0300

    Update consultant.py

--- consultant.py
+++ consultant.py
@@ -1,6 +1,9 @@
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.metrics.pairwise import cosine_similarity
 import json
+import pymorphy2
+morph = pymorphy2.MorphAnalyzer()
+
 def index_of_max(lst):
     maxi = 0
     for i in range(len(lst)):
@@ -16,13 +19,21 @@ class ArtificialIntelligence:
         self.vectorizer = TfidfVectorizer()
         self.docs_tfidf = self.vectorizer.fit_transform(self.base)
     def get_similarity(self, query):
-        query = self.filter_of_query(query)
+        query = self.lemmatize(self.filter_of_query(query))
         query_tfidf = self.vectorizer.transform([query])
         return self.base[index_of_max(list(cosine_similarity(query_tfidf, self.docs_tfidf).flatten()))]
     def filter_of_query(self, query):
         stop_words = ["не", "без", "через", "у", "но", "о", "и", "как", "python", "а", "к", "в", "с", "за", "или", "либо", "же", "ж", "совсем", "ничуть", "отнюдь", "перед", "при", "про", "под", "по", "до", "чуть", "чуть-чуть"]
         return " ".join([i for i in query.split(" ") if not i in stop_words])
+    def lemmatize(self, text):
+        words = text.split() # разбиваем текст на слова
+        res = list()
+        for word in words:
+            p = morph.parse(word)[0]
+            res.append(p.normal_form)
 
-q = input()
-a = ArtificialIntelligence("lessons.json")
-print("Найдено:", a.get_similarity(q))
+        return res
+
+# q = input()
+# a = ArtificialIntelligence("lessons.json")
+# print("Найдено:", a.get_similarity(q))
