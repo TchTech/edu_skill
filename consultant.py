2ea9616c1697b288985295d8fef1ceb59e5fc234
TchTech <67865526+TchTech@users.noreply.github.com>
2023-03-26 12:33:15 +0300

    Update consultant.py

--- consultant.py
+++ consultant.py
@@ -1,8 +1,5 @@
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.metrics.pairwise import cosine_similarity
-import nltk
-nltk.download('stopwords')
-from nltk.corpus import stopwords
 import json
 def index_of_max(lst):
     maxi = 0
@@ -12,13 +9,12 @@ def index_of_max(lst):
     return maxi
 class ArtificialIntelligence:
     def __init__(self, name_of_file):
-        russian_stopwords = stopwords.words("russian")
         with open(name_of_file, "r", encoding="utf-8") as f:
             doc = dict(json.load(f))
             self.base = []
             [self.base.extend(i) for i in [list(i.values()) for i in list(doc.values())]]
-        self.vectorizer = TfidfVectorizer(stop_words=russian_stopwords)
-        self.docs_tfidf = self.vectorizer.fit_transform(b)
+        self.vectorizer = TfidfVectorizer()
+        self.docs_tfidf = self.vectorizer.fit_transform(self.base)
     def get_similarity(self, query):
         query_tfidf = self.vectorizer.transform([query])
         return self.base[index_of_max(list(cosine_similarity(query_tfidf, self.docs_tfidf).flatten()))]
